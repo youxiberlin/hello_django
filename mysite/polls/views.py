18b06bbac1267030af7db3db01c5b8d6001ff764
Yuki Sato <yuki.sato@lightcurve.io>
2020-05-28 09:10:42 +0200

    Show list of questions in poll view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello world. you are at the polls index")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
 
 def detail(request, question_id):
 	response = "You are looking at the results of question %s."
